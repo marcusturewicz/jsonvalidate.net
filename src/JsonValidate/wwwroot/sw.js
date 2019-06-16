9eda654653e94bcc3d466297b63bde011074cac7
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-06-16 10:45:09 +1000

    Adds manifest.json and icons

--- src/JsonValidate/wwwroot/sw.js
+++ src/JsonValidate/wwwroot/sw.js
@@ -37,6 +37,14 @@ self.addEventListener('install', e => {
        '/sw.js',
        '/favicon.ico',
        '/index.html',
+       '/images/icons/icon-72x72.png',
+       '/images/icons/icon-96x96.png',
+       '/images/icons/icon-128x128.png',
+       '/images/icons/icon-144x144.png',
+       '/images/icons/icon-152x152.png',
+       '/images/icons/icon-192x192.png',
+       '/images/icons/icon-384x384.png',
+       '/images/icons/icon-512x512.png',
 
        // CDN files
        'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css',
