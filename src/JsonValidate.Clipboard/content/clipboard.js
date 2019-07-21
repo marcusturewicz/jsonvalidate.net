1bb21aeae1331de6ae4db3cf4931a6aea6a2a555
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-07-21 15:27:01 +1000

    Support copy on Safari and Edge
    Fixes #50

--- src/JsonValidate.Clipboard/content/clipboard.js
+++ src/JsonValidate.Clipboard/content/clipboard.js
@@ -1,5 +1,14 @@
 window.clipboard = {
   copy: function (text) {
+    if (!navigator.clipboard) {
+      var textArea = document.createElement("textarea");
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      document.execCommand('copy');
+      return new Promise(() => {});
+    }
     return navigator.clipboard.writeText(text);
   }
 };
