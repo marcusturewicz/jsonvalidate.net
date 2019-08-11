cdd90339db6e55139b063cd8fafc4c5dbf8eb406
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-08-11 17:41:35 +1000

    Update clipboard.js to support iOS
    Credit https://stackoverflow.com/a/34046084/6939988
    
    Fixes #52

--- src/JsonValidate.Clipboard/content/clipboard.js
+++ src/JsonValidate.Clipboard/content/clipboard.js
@@ -1,13 +1,33 @@
 window.clipboard = {
   copy: function (text) {
     if (!navigator.clipboard) {
-      var textArea = document.createElement("textarea");
-      textArea.value = text;
-      document.body.appendChild(textArea);
-      textArea.focus();
-      textArea.select();
+      var el = document.createElement("textarea");
+      el.value = text;
+      document.body.appendChild(el);
+      el.focus();
+      el.select();
+
+      var oldContentEditable = el.contentEditable,
+      oldReadOnly = el.readOnly,
+      range = document.createRange();
+
+      el.contentEditable = true;
+      el.readOnly = false;
+      range.selectNodeContents(el);
+
+      var s = window.getSelection();
+      s.removeAllRanges();
+      s.addRange(range);
+
+      el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+      el.contentEditable = oldContentEditable;
+      el.readOnly = oldReadOnly;
+
       document.execCommand('copy');
-      textArea.remove();
+
+      el.remove();
+     
       return new Promise(() => {});
     }
     return navigator.clipboard.writeText(text);
