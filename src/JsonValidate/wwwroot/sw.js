8c3d757ee68c407b8f265f00434cedc7f10d6d83
marcusturewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-08-14 18:03:08 +1000

    Remove console write

--- src/JsonValidate/wwwroot/sw.js
+++ src/JsonValidate/wwwroot/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'jsonvalidate-v2.0.0';
+var cacheName = 'jsonvalidate-v2.1.1';
 self.addEventListener('install', e => {
     e.waitUntil(
       caches.open(cacheName).then(cache => {
