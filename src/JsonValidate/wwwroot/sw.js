6ee3cc5509372e6414d957ad1f5182a09381a49e
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2019-12-09 10:34:32 +1100

    Update sw.js

--- src/JsonValidate/wwwroot/sw.js
+++ src/JsonValidate/wwwroot/sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'jsonvalidate-v2.6.2';
+var cacheName = 'jsonvalidate-v2.6.3';
 self.addEventListener('install', e => {
     e.waitUntil(
       caches.open(cacheName).then(cache => {
